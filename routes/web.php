cef7a4ef1df680ecc4eba18d8e64e5f7d5204fb2
qasimr <qasimr@icloud.com>
2023-03-09 18:37:56 +0100

    github.php route zu github abgeändert

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/github.php', function () {
+Route::get('/github', function () {
     return view('github');
 });
