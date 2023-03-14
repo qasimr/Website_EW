46d507a631088f0d1a00fc1c8a60930c5a3e4659
qasimr <qasimr@icloud.com>
2023-03-14 22:22:30 +0100

    Titel geändert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Test Twill - ELLA W</title>
+        <title>Test ELLA W</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
